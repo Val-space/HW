3b4b3732eed7ed373483281b680ea059dc46dd86
Lera <example@gmail.com>
2021-05-28 21:22:50 +0300

    commit

---
+++ hw_8/script.js
@@ -0,0 +1,41 @@
+function Auto(make, model, year, vinCode) {
+    this.make = make,
+        this.model = model,
+        this.year = year,
+        this.vinCode = vinCode
+}
+
+Auto.prototype.log = function () {
+    console.log(this.make + ' ' + this.model + ' ' + this.year);
+}
+Auto.prototype.checkVin = function () {
+    return this.vinCode.length === 16 ? true : false
+}
+
+
+function Auto_Fuel(make, model, year, vinCode, engineCapacity, fuelConsumption) {
+    this.make = make,
+        this.model = model,
+        this.year = year,
+        this.vinCode = vinCode,
+        this.engineCapacity = engineCapacity,
+        this.fuelConsumption = fuelConsumption
+}
+Auto_Fuel.prototype = Object.create(Auto.prototype)
+Auto_Fuel.prototype.showFuelConsumption = function () {
+    return this.engineCapacity + this.fuelConsumption
+}
+
+
+
+function Auto_Electric(make, model, year, vinCode, batteryCapacity) {
+    this.make = make,
+        this.model = model,
+        this.year = year,
+        this.vinCode = vinCode,
+        this.batteryCapacity = batteryCapacity
+}
+Auto_Electric.prototype = Object.create(Auto.prototype)
+Auto_Electric.prototype.showBatteryConfig = function () {
+    return this.batteryCapacity
+}

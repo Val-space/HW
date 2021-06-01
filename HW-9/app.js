80e8f4be360187c5f157583f27eed5c536da6393
Lera <example@gmail.com>
2021-06-01 21:21:34 +0300

    hw

---
+++ HW-9/app.js
@@ -0,0 +1,38 @@
+let date = 20200302;
+
+let inputData = document.querySelector('.input');
+inputData.addEventListener('change', () => {
+    date = moment(inputData.value).format("YYYYMMDD");
+    // console.log(date)
+    getData(date)
+})
+
+const renderCurrencies = curr => {
+    console.log(curr);
+    let htmlStr = '';
+    for (let i of curr) {
+        htmlStr += `<tr>
+                <td>${i.txt}</td>
+                <td>${i.rate}</td>
+            </tr>`;
+    }
+    document.querySelector('table > tbody').innerHTML = htmlStr;
+
+}
+
+
+let currenciesInfo = [];
+const getData = (date) => {
+    console.log(date)
+    fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=${date}&json`)
+        .then(res => res.json()).then(data => {
+            currenciesInfo = data.map(currency => {
+                return {
+                    txt: currency.txt,
+                    rate: currency.rate
+                }
+            })
+            renderCurrencies(currenciesInfo);
+        });
+}
+getData(date);
